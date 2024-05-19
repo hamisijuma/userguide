b406cab135498713c1192a50ddee50d911bb6fc1
hamisijuma <hamisibnj95@gmail.com>
2024-05-19 13:56:00 +0300

    Add files via upload

---
+++ videos.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Use title if it's in the page YAML frontmatter -->
+    <title>GRENEBOOKS</title>
+
+    <meta name="description" content="Grenebooks ni mfumo bora wa kusimamia mauzo na stock." />
+    <meta name="keywords" content="Grenebooks, Pos, system, Mfumo wa mauzo, User guide, Tanzania" />
+
+    <link href="/h2/stylesheets/normalize.css" rel="stylesheet" type="text/css" /><link href="/h2/stylesheets/all.css" rel="stylesheet" type="text/css" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/3.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="style.css" />
+    <!-- Boxicons CDN Link -->
+    <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
+
+    <script src="/h2/javascripts/modernizr.js" type="text/javascript"></script>
+
+
+    <link href="/h2/images/lukinet-logo.png" rel="icon" type="image/png" />
+
+
+  </head>
+
+  <body class="index">
+    <header class="header contain-to-grid">
+      <nav class="top-bar">
+        <ul class="title-area" >
+          <li class="name" style="padding-left: 200px;">
+            <h1><a href="/h2/index.html">Lukinet</a></h1>
+          </li>
+        </ul>
+            <section class="top-bar-section">
+          <!-- Left Nav Section -->
+          <ul class="left">
+              <li class="item "><a href="/h2/faq.html">FAQs</a></li>
+              <li class="item "><a href="/h2/howto.html">Yaliyomo</a></li>
+              <li class="item "><a href="/h2/videos.php">Video</a></li>
+              <li class="item "><a href="https://www.demo.grenebooks.com/login">Demo</a></li>
+          </ul>
+        </section>
+      </nav>
+    </header>
+
+      <!-- Left Nav Section -->
+      <div class="sidebar">
+        <ul class="nav-links">
+          <li>
+            <a href="#" class="active">
+              <i class="bx bx-grid-alt"></i>
+              <span class="links_name">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-box"></i>
+              <span class="links_name">Product</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="bx bx-list-ul"></i>
+              <span class="links_name">Order list</span>
+            </a>
+          </li>
+          </div>
+        </section>
+ 
+
